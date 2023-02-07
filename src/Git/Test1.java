800fecd11195d09ea16b0070cc4b3b09df5e3a1b
BABAPEER <BABAPEER@DESKTOP-8L1U83C>
2023-02-07 11:02:07 +0530

    first commit......

---
+++ src/Git/Test1.java
@@ -0,0 +1,24 @@
+package Git;
+
+public class Test1 {
+
+	public static void main(String[] args)
+	{
+		String hungry ="yes";
+		if(hungry == "yes")
+		{
+			System.out.println("Eat something");
+			
+			
+			
+		}
+		
+		else
+		{
+			System.out.println("Drink water");
+		}
+		
+
+	}
+
+}
